bccb9a0fb3ea8661812ad579a358e1ac648769a1
Tarn Yeong Ching <i@ctarn.io>
2023-03-23 15:11:50 +0800

    add `read_cross_link_pair`

--- src/io/extra.jl
+++ src/io/extra.jl
@@ -8,3 +8,19 @@ read_precursor(input) = begin
     end
     return I
 end
+
+"`input` accept the same types as `CSV.File` including file path and `IO`"
+read_cross_link_pair(input) = begin
+    T_v = typeof((idx=0, mz=0.0, z=0, pairs=[(0.0, 0.0)]))
+    P = Dict{Int, Vector{T_v}}()
+    for r in CSV.File(input)
+        v = get!(P, r.scan, T_v[])
+        i = findfirst(x -> x.idx == r.prec, v)
+        if isnothing(i)
+            push!(v, (; idx=r.prec, r.mz, r.z, pairs=typeof((0.0, 0.0))[]))
+            i = length(v)
+        end
+        push!(v[i].pairs, (r.a, r.b))
+    end
+    return P
+end
